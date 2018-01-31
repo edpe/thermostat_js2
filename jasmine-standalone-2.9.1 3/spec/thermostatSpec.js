c2028e59f8e7001bf7fb60cb0c096b39e4953326
samuelmaker <samuelwood.co@gmail.com>
2018-01-31 16:40:22 +0000

    test for maximum temp with powersaving mode

--- jasmine-standalone-2.9.1 3/spec/thermostatSpec.js
+++ jasmine-standalone-2.9.1 3/spec/thermostatSpec.js
@@ -33,4 +33,18 @@ describe("Thermostat", function () {
     expect( function() { thermostat.up() }).toThrow(new Error(error))
   })
 
+  it("max temp is 32 when power saving mode is off", function() {
+    thermostat.powerSaving(false);
+    error = "maximum tempreture reached"
+    thermostat.temp = 32
+    expect( function() { thermostat.up() }).toThrow(new Error(error))
+  })
+
+  it("can set temp to 32 when power saving mode is off", function() {
+    thermostat.powerSaving(false);
+    thermostat.temp = 31
+    thermostat.up()
+    expect(thermostat.temp()).toEqual(32)
+  })
+
 })
