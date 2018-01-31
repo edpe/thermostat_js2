c35ebf5bb72197186684c10ce1d2f9d1dc6bd211
Tagrand <thomas.a.grand@gmail.com>
2018-01-31 16:28:10 +0000

    add power saving check in up method

--- jasmine-standalone-2.9.1 3/src/thermostat.js
+++ jasmine-standalone-2.9.1 3/src/thermostat.js
@@ -1,18 +1,25 @@
 const DEFAULT_TEMP = 20;
 const MIN_TEMP = 10;
+const MAX_TEMP = 25;
 
 function Thermostat () {
   this.temp = DEFAULT_TEMP;
-
+  this.mode = false;
 };
 
 Thermostat.prototype = {
 
-    up: function() { this.temp++; },
+    up: function() {
+      if(this.temp >= MAX_TEMP) {throw new Error("maximum tempreture reached")}
+      this.temp++; },
 
     down: function() {
        this._minTemp();
        this.temp--;
+    },
+
+    powerSaving: function(mode) {
+       this.mode = mode;
     },
 
     _minTemp: function() {
